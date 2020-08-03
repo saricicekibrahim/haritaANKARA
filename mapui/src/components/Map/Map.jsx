b927f15f4985120c0a6e23e06171c98c3a71c87a
saricicek-pc <ibrahimsaricicek@gmail.com>
2020-08-03 11:46:43 +0300

    added custom raster layer

--- mapui/src/components/Map/Map.jsx
+++ mapui/src/components/Map/Map.jsx
@@ -9,18 +9,33 @@ class Map extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lng: 5,
-            lat: 34,
-            zoom: 2
+            lng: 32.859,
+            lat: 39.938,
+            zoom: 13.99
         };
     }
 
     componentDidMount() {
         const map = new mapboxgl.Map({
             container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
+            style: 'mapbox://styles/mapbox/satellite-v9',
             center: [this.state.lng, this.state.lat],
             zoom: this.state.zoom
+        });
+
+        map.on('load', function () {
+            map.addSource('mapbox://ibrahimsaricicek.9ngn7hoy', {
+                "url": "mapbox://ibrahimsaricicek.9ngn7hoy",
+                "type": "raster",
+                "tileSize": 256
+            });
+            map.addLayer({
+                "id": "ibrahimsaricicek-1924",
+                "type": "raster",
+                "source": "mapbox://ibrahimsaricicek.9ngn7hoy",
+                'minzoom': 0,
+                'maxzoom': 22
+            });
         });
 
         map.on('move', () => {
